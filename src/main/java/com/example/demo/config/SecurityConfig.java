efc9b12e5469965a3e440a14ec7956236502b33f
Senkitsu <bigflow621@gmail.com>
2025-11-11 17:48:40 +0300

    Исправил ошибку при контакте springsecurity и swagger

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,58 +2,29 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.example.demo.jwt.JwtAuthEntryPoint;
-import com.example.demo.jwt.JwtAuthFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-    
-    private final JwtAuthFilter jwtAuthFilter;
-    private final JwtAuthEntryPoint jwtAuthEntryPoint;
 
     @Bean
-    public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorize -> {
-                authorize.requestMatchers("/api/auth/login").permitAll();
-                authorize.requestMatchers("/api/auth/refresh").permitAll();
-                authorize.anyRequest().authenticated();
-            })
-            .sessionManagement(session -> session.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS))
-            .exceptionHandling(exception ->
-                 exception.authenticationEntryPoint(jwtAuthEntryPoint))
-            .addFilterBefore(jwtAuthFilter, 
-                UsernamePasswordAuthenticationFilter.class);
-
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers(
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui.html",
+                    "/webjars/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
-    }
-
-    @Bean
-    static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
